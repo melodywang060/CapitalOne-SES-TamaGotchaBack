40cf12fcf91174bca91bf1a753917a59ad28c2a0
celimardeida <celimar.deida@upr.edu>
2022-08-05 09:35:29 -0400

    home credit score

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className="footer">
             <div className="footerlinks">
                 <Link to="/budget"><div className="font-face-gm">Budget Remaining: <b>$2,500.00</b></div></Link>
-                <Link to="/creditscore"><div className="font-face-gm">Credit Score: <r>300</r></div></Link>
+                <Link to="/creditscore"><div className="font-face-gm">Credit Score: <b>300</b></div></Link>
             </div>
         </div>
     );
